fbed6cfc0b33cd5eed409180e9ff5defec3d1370
ZTiKnl <ZTiK.nl@gmail.com>
2020-02-13 03:56:34 +0100

    added site title to config options

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 $siteurl = 'https://ida-bgs.ztik.nl/';
+$sitetitle = 'Independent Defence Agency - BGS';
 $securedbcreds = '../private/db.inc.php';
 
 $highlight_pmfname = 'Independent Defence Agency';
