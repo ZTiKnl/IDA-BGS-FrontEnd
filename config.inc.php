177c108c2e07df65550d5c717f356042d8d53620
ZTiKnl <ZTiK.nl@gmail.com>
2020-02-15 23:08:50 +0100

    set site scripts to default london timezone, added systeminfluence warning percentage

--- config.inc.php
+++ config.inc.php
@@ -1,8 +1,11 @@
 <?php
+date_default_timezone_set("Europe/London");
 
 $siteurl = 'https://ida-bgs.ztik.nl/';
 $sitetitle = 'Independent Defence Agency - BGS';
 $securedbcreds = '../private/db.inc.php';
 
 $highlight_pmfname = 'Independent Defence Agency';
+
+$systeminfluencewarningpercentage = 1;
 ?>
