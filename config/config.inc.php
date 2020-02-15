382f41c56e003051464af5d2c32ebaa3bf79d1cf
ZTiKnl <ZTiK.nl@gmail.com>
2020-02-16 00:36:14 +0100

    renamed from config.inc.php

---
+++ config/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+date_default_timezone_set("Europe/London");
+
+$siteurl = 'https://ida-bgs.ztik.nl/';
+$sitetitle = 'Independent Defence Agency - BGS';
+
+$securedbcreds = '../private/db.inc.php';
+$siteincludefiles = 'script-includes/';
+
+$pmfname = 'Independent Defence Agency';
+$pmfshortname = 'IDA';
+
+$systeminfluencewarningpercentage = 1;
+?>
