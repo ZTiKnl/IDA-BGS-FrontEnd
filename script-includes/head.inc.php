f3c9599222e85e52886c227fde175102bda5b68c
ZTiKnl <ZTiK.nl@gmail.com>
2020-02-17 23:10:30 +0100

    first commit

---
+++ script-includes/head.inc.php
@@ -0,0 +1,11 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?PHP echo $sitetitle; ?></title>
+  <link rel="stylesheet" href="<?PHP echo $siteurl; ?>css/style.css" type="text/css">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?PHP echo $siteurl; ?>images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?PHP echo $siteurl; ?>images/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?PHP echo $siteurl; ?>images/favicon-16x16.png">
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript" src="<?PHP echo $siteurl.$siteincludefiles; ?>functions.inc.js"></script>
+</head>
